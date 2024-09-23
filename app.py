58916b3c4e8c250798aa423e59dbaa85b4c1e379
Luca Paganotto <luca.paganotto1@gmail.com>
2024-09-23 15:10:15 +0200

    /prime done

--- app.py
+++ app.py
@@ -28,13 +28,23 @@ async def kelToCel(kelvin: float):
     }
 
 
+#zahl hat divisor der kleiner oder gleich quadratwurzel der zahl entspricht
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1): #itteriert durch alle zahlen i von 2 bis zur quadratwurzel von num
+        if num % i == 0:
+            return False
+    return True
 
-@app.get("/tempkel/{kelvin}") #eingabe der Zahl für die Umrechung
-async def kelToCel(kelvin: float):
-    celsius_sub = 273.15
-    result = kelvin - celsius_sub #subtrahiere 275.15 erhalte celsius
+@app.get("/prime/{limit}")
+async def sieve_of_eratosthenes(limit: int):
+    is_prime = [True] * (limit + 1)
+    is_prime[0], is_prime[1] = False, False
     
-    return {
-        "kelvin": kelvin, 
-        "celsius": result        
-    }
+    for num in range(2, int(limit**0.5) + 1):
+        if is_prime[num]:
+            for multiple in range(num*num, limit + 1, num):
+                is_prime[multiple] = False
+    
+    return[num for num, prime in enumerate(is_prime) if prime]
