99427e77d41fa93982d57c35de59cfdefcd97b7b
Luca Paganotto <luca.paganotto1@gmail.com>
2024-09-27 14:13:11 +0200

    deletion of is_prime function

--- app.py
+++ app.py
@@ -7,20 +7,21 @@ async def api():
     return {"message": "Hello World"}
 
 #Aufgabe 1
-@app.get("/tempcel/{celsius}") #eingabe der Zahl für die Umrechung
+#Einfache addition für die umrechnung
+@app.get("/tempcel/{celsius}") 
 async def celToKel(celsius: float):
     kelvin_add = 273.15
-    result = celsius + kelvin_add #addiere 275.15 erhalte kelvin
+    result = celsius + kelvin_add #addieren der Zahl 275.15 um kelvin zu erhalten
     
     return {
         "celsius": celsius,
         "kelvin": result
     }
 
-@app.get("/tempkel/{kelvin}") #eingabe der Zahl für die Umrechung
+@app.get("/tempkel/{kelvin}")
 async def kelToCel(kelvin: float):
     celsius_sub = 273.15
-    result = kelvin - celsius_sub #subtrahiere 275.15 erhalte celsius
+    result = kelvin - celsius_sub #subtraktion der Zahl 275.15 um Celsius zu erhalten
     
     return {
         "kelvin": kelvin, 
@@ -28,15 +29,6 @@ async def kelToCel(kelvin: float):
     }
 
 #Aufgabe 2
-#zahl hat divisor der kleiner oder gleich quadratwurzel der zahl entspricht
-def is_prime(num):
-    if num <= 1:
-        return False
-    for i in range(2, int(num**0.5) + 1): #itteriert durch alle zahlen i von 2 bis zur quadratwurzel von num
-        if num % i == 0:
-            return False
-    return True
-
 @app.get("/prime/{limit}")
 async def sieve_of_eratosthenes(limit: int):
     is_prime = [True] * (limit + 1)
@@ -53,7 +45,7 @@ async def sieve_of_eratosthenes(limit: int):
 @app.get("/number/{limit}")
 async def fib(limit: int, a=0, b=1):
     n = limit
-    a, b = 0, 1 #beiden ersten fib nummern
+    a, b = 0, 1 #definiert die ersten beiden fibonacci nummern
 
     for _ in range(1, n+1): #fib wird standartmässig bei 0 angefangen aber hier bei 1 deswegen n+1
         a, b = b, a+b
